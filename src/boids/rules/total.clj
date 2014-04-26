f5e69dfde898f00b4698aa514dcb7a22bc8541d2
Arkadiy Kukarkin <arkadiy@foursquare.com>
2014-04-26 16:27:59 -0400

    Mark vars as dynamic for Clojure 1.5.1

--- src/boids/rules/total.clj
+++ src/boids/rules/total.clj
@@ -6,26 +6,26 @@
 	boids.rules.avoidance
 	boids.rules.bounds))
 
-(def *center-of-mass-weight* 0.01)
-(def *velocity-weight* 1.0)
-(def *avoidance-weight* 1.0)
-(def *bounds-weight* 1.0)
-(def *goal-weight* 1.0)
-(def *bounds-radius* 0.0)
-(def *avoidance-radius* 2.0)
+(def ^:dynamic *center-of-mass-weight* 0.01)
+(def ^:dynamic *velocity-weight* 1.0)
+(def ^:dynamic *avoidance-weight* 1.0)
+(def ^:dynamic *bounds-weight* 1.0)
+(def ^:dynamic *goal-weight* 1.0)
+(def ^:dynamic *bounds-radius* 0.0)
+(def ^:dynamic *avoidance-radius* 2.0)
 
-(defn total-adjustment 
+(defn total-adjustment
   ([the-boid all-boids bounds]
      (let [c (sv-mul *center-of-mass-weight* (center-of-mass-adjustment the-boid all-boids))
 	   v (sv-mul *velocity-weight* (velocity-adjustment the-boid all-boids))
-	   a (sv-mul *avoidance-weight* 
+	   a (sv-mul *avoidance-weight*
 		     (avoidance-adjustment the-boid all-boids *avoidance-radius*))
 	   b (sv-mul *bounds-weight* (bounds-adjustment the-boid bounds *bounds-radius*))]
        (sv-sum c v a b (:velocity the-boid))))
   ([the-boid all-boids bounds the-goal]
      (let [c (sv-mul *center-of-mass-weight* (center-of-mass-adjustment the-boid all-boids))
 	   v (sv-mul *velocity-weight* (velocity-adjustment the-boid all-boids))
-	   a (sv-mul *avoidance-weight* 
+	   a (sv-mul *avoidance-weight*
 		     (avoidance-adjustment the-boid all-boids *avoidance-radius*))
 	   b (sv-mul *bounds-weight* (bounds-adjustment the-boid bounds *bounds-radius*))
 	   g (sv-mul *goal-weight* (goal-adjustment the-boid the-goal))]
